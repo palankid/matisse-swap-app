2ed48c07c05639df4c4cb70e8b956dd295fe8a0d
Daniel Palanki <daniel.palanki79@gmail.com>
2022-02-19 22:01:41 +0100

    chore: Add redux toolkit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,16 @@
 import * as React from "react";
 import type { NextPage } from "next";
+import { Provider } from "react-redux";
 
 import App from "features";
+import store from "store";
 
 const Home: NextPage = () => {
-  return <App />;
+  return (
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
 };
 
 export default Home;
